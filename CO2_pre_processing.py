bafdc2ced06ec778a24752d198411f66f6543075
hectorLop <lopez.almazan.hector@gmail.com>
2020-06-11 17:58:51 +0200

    Added function prepare_timeseries_dataset

--- CO2_pre_processing.py
+++ CO2_pre_processing.py
@@ -75,30 +75,30 @@ class CO2DataPreparation():
 
         return datetime(year, month, day, hour)
 
+    def prepare_timeseries_dataset(self, dataset: pandas.DataFrame, ts_column_position: int) -> pandas.DataFrame:
+        """
+        Prepare a time series dataset ready to be processed
+
+        Parameters:
+            dataset (pandas.DataFrame): dataset to be prepared
+            ts_column_pos (int): position of the timestamp column
+        Returns:
+            dataset ready to be processed
+        """
+
+        ts_column_name = dataset.columns[ts_column_position]
+
+        # Converts the timestamp column to datetime object
+        dataset[ts_column_name] = pandas.to_datetime(dataset[ts_column_name])
+        # Sets the timestamp column as index
+        dataset = dataset.set_index(ts_column_name)
+        # Sort the dataset by index
+        dataset = dataset.sort_index()
+
+        return dataset
 
 
 
-def prepare_timeseries_dataset(dataset: pandas.DataFrame, ts_column_position: int) -> pandas.DataFrame:
-    """
-    Prepare a time series dataset ready to be processed
-
-    Parameters:
-        dataset (pandas.DataFrame): dataset to be prepared
-        ts_column_pos (int): position of the timestamp column
-    Returns:
-        dataset ready to be processed
-    """
-
-    ts_column_name = dataset.columns[ts_column_position]
-
-    # Converts the timestamp column to datetime object
-    dataset[ts_column_name] = pandas.to_datetime(dataset[ts_column_name])
-    # Sets the timestamp column as index
-    dataset = dataset.set_index(ts_column_name)
-    # Sort the dataset by index
-    dataset = dataset.sort_index()
-
-    return dataset
 
 
 
