52cc53707a57eb347be94f5d187296ad1c408b12
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-16 11:15:16 +0200

    Added RemoveDuplicates transformer

--- Transformers/cleaning_transformers.py
+++ Transformers/cleaning_transformers.py
@@ -1,14 +1,57 @@
 from sklearn.base import TransformerMixin
+import pandas
 
 class RemoveDuplicates(TransformerMixin):
-    def __init__(self) -> None:
-        pass
+    """
+    This class defines a Transformer to remove duplicated dates
 
-    def fit(self, X, y=None):
-        pass
+    Parameters
+    ----------
+    column_name : string
+        Column name which contains duplicates
 
-    def transform(self, X, y=None):
-        pass
+    Attributes
+    ----------
+    column_name : string
+        Column name which contains duplicates
+    """
+
+    def __init__(self, column_name: str) -> None:
+        self._column_name = column_name
+
+    def fit(self, X: pandas.DataFrame, y=None):
+        """
+        Standard behaviour for fit methods
+
+        Parameters
+        ----------
+        X : pandas.DataFrame
+            Dataframe with the data
+        
+        Returns
+        -------
+        self : RemoveDuplicates
+            Self object
+        """
+        return self
+
+    def transform(self, X: pandas.DataFrame) -> pandas.DataFrame:
+        """
+        Drop the duplicated dates from the given data
+
+        Parameters
+        ----------
+        X : pandas.DataFrame
+            Dataframe with the data
+        
+        Returns
+        -------
+        X : pandas.DataFrame
+            DataFrame containing no duplicated dates
+        """
+        X = X.drop_duplicates(self._column_name)
+
+        return X
 
 class RemoveErrors(TransformerMixin):
     def __init__(self) -> None:
