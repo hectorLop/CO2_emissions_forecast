ad3bf983a3bbc75ac73a2a4835a27bfceec49402
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-16 10:28:02 +0200

    Added skeleton for cleaning custom transformers

---
+++ Transformers/cleaning_transformers.py
@@ -0,0 +1,21 @@
+from sklearn.base import TransformerMixin
+
+class RemoveDuplicates(TransformerMixin):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y=None):
+        pass
+
+    def transform(self, X, y=None):
+        pass
+
+class RemoveErrors(TransformerMixin):
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y=None):
+        pass
+
+    def transform(self, X, y=None):
+        pass
