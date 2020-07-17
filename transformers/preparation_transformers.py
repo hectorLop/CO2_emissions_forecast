52549b8c6d053ae8a0554afd222fbfe421c12cc2
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-17 17:42:11 +0200

    Added ConvertToDatetime transformer

--- transformers/preparation_transformers.py
+++ transformers/preparation_transformers.py
@@ -3,14 +3,57 @@ from sklearn.base import TransformerMixin
 import pandas
 
 class ConvertToDatetime(TransformerMixin):
-    def __init__(self) -> None:
-        pass
+    """
+    This class defines a Transformer to convert the date's column elements from
+    a string to a datetime object
+
+    Parameters
+    ----------
+    column_name : string
+        Name of the column containing the dates
+
+    Attributes
+    ----------
+    column_name : string
+        Name of the column containing the dates
+    """
+
+    def __init__(self, column_name: str) -> None:
+        self._column_name = column_name
 
     def fit(self, X: pandas.DataFrame, y=None) -> ConvertToDatetime:
-        pass
+        """
+        Standard behaviour for fit methods
+
+        Parameters
+        ----------
+        X : pandas.DataFrame
+            Dataframe with the data
+        
+        Returns
+        -------
+        self : RemoveDuplicates
+            Self object
+        """
+        return self
 
     def transform(self, X: pandas.DataFrame) -> pandas.DataFrame:
-        pass
+        """
+        Convert the date's column elements from string to datetime
+
+        Parameters
+        ----------
+        X : pandas.DataFrame
+            DataFrame with the data
+
+        Returns
+        -------
+        X : pandas.DataFrame
+            DataFrame with the date's column elements as datetime objects 
+        """
+        X[self._column_name] = pandas.to_datetime(X[self._column_name])
+
+        return X
 
 class SortByIndex(TransformerMixin):
     def __init__(self) -> None:
