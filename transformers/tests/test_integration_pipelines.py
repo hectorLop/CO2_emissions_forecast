08a6e596e783f470140867b88f74f89adf172536
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-23 10:06:04 +0200

    Added fixture to supply cleaning and preparation pipelines

--- transformers/tests/test_integration_pipelines.py
+++ transformers/tests/test_integration_pipelines.py
@@ -5,14 +5,41 @@ import pandas
 from pandas.testing import assert_frame_equal
 from sklearn.pipeline import Pipeline
 
-def test_cleaning_pipeline():
+@pytest.fixture
+def supply_pipelines() -> dict:
     """
-    Test the full cleaning pipeline
+    Provide a dictionary containing the cleaning and the preparation pipelines
     """
     cleaning_pipeline = Pipeline([
         ('remove_duplicates', RemoveDuplicates('Dates')),
         ('remove_errors', RemoveDateErrors('Dates'))
     ])
+
+    preparation_pipeline = Pipeline([
+        ('convert_to_datetime', ConvertToDatetime('Dates')),
+        ('sort_by_index', SortByIndex('Dates')),
+        ('set_frequency', SetFrequency('10min')),
+        ('interpolation', Interpolation()),
+        ('resampler', Resampler('H', 'Emissions')),
+        ('boxcox', BoxCox('Emissions'))
+    ])
+
+    pipelines = {}
+    pipelines['cleaning'] = cleaning_pipeline
+    pipelines['preparation'] = preparation_pipeline
+
+    return pipelines
+
+def test_cleaning_pipeline(supply_pipelines):
+    """
+    Test the full cleaning pipeline
+
+    Parameters
+    ----------
+    supply_pipelines : dict
+        Dictionary containing the cleaning pipeline
+    """
+    cleaning_pipeline = supply_pipelines['cleaning']
 
     original_df = pandas.DataFrame({
         'Dates': ['2020-01-01 01:00', '2020-01-01 01:10',
@@ -45,18 +72,16 @@ def test_cleaning_pipeline():
 
     assert_frame_equal(cleaned_data, expected_df)
 
-def test_preparation_pipeline():
+def test_preparation_pipeline(supply_pipelines):
     """
     Test the full preparation pipeline
+
+    Parameters
+    ----------
+    supply_pipelines : dict
+        Dictionary containing the preparation pipeline
     """
-    preparation_pipeline = Pipeline([
-        ('convert_to_datetime', ConvertToDatetime('Dates')),
-        ('sort_by_index', SortByIndex('Dates')),
-        ('set_frequency', SetFrequency('10min')),
-        ('interpolation', Interpolation()),
-        ('resampler', Resampler('H', 'Emissions')),
-        ('boxcox', BoxCox('Emissions'))
-    ])
+    preparation_pipeline = supply_pipelines['preparation']
 
     original_df = pandas.DataFrame({
         'Dates': ['2020-01-01 01:00', '2020-01-01 01:10',
@@ -77,3 +102,34 @@ def test_preparation_pipeline():
     prepared_df = preparation_pipeline.fit_transform(original_df)
 
     assert_frame_equal(prepared_df, expected_df)
+
+def test_cleaning_and_preparation_pipelines():
+    """
+    Test both the cleaning and preparation pipeliness
+    """
+    cleaning_pipeline = Pipeline([
+        ('remove_duplicates', RemoveDuplicates('Dates')),
+        ('remove_errors', RemoveDateErrors('Dates'))
+    ])
+
+    preparation_pipeline = Pipeline([
+        ('convert_to_datetime', ConvertToDatetime('Dates')),
+        ('sort_by_index', SortByIndex('Dates')),
+        ('set_frequency', SetFrequency('10min')),
+        ('interpolation', Interpolation()),
+        ('resampler', Resampler('H', 'Emissions')),
+        ('boxcox', BoxCox('Emissions'))
+    ])
+
+    original_df = pandas.DataFrame({
+        'Dates': ['2020-01-01 01:00', '2020-01-01 01:10',
+                '2020-01-01 01:10', '2020-01-01 01:20',
+                '2020-01-01 01:20', '2020-01-01 01:30',
+                '2020-01-01 01:40', '2020-01-01 01:50',
+                '2020-01-01 2A:00', '2020-01-01 2A:10',
+                '2020-01-01 2A:20', '2020-01-01 2A:30',
+                '2020-01-01 2A:40', '2020-01-01 2A:50',
+                '2020-01-01 2B:00', '2020-01-01 2B:10'],
+        'Emissions': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                    12, 13, 14, 15, 16]
+    })
