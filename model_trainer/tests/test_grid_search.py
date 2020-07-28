8d0bf9ed45d9504bbe7cd53a2d00c2624d86b684
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-28 11:22:18 +0200

    Added test for ProphetGridSearch

--- model_trainer/tests/test_grid_search.py
+++ model_trainer/tests/test_grid_search.py
@@ -1,13 +1,14 @@
 import pytest
 import pandas
-from ..grid_search import ARIMAGridSearch
+from ..grid_search import ARIMAGridSearch, ProphetGridSearch
 
 @pytest.fixture
 def supply_df() -> pandas.DataFrame:
     """
     Supply a test dataframe
     """
-    df = pandas.read_csv('../../Test_Dataset_2020_01_01_to_2020_02_29.csv')
+    # Use this path due to the tests are executed from the main directory
+    df = pandas.read_csv('Test_Dataset_2020_01_01_to_2020_02_29.csv')
 
     # Converts the dates column to datetime object and sets it as the index 
     df['Fecha'] = pandas.to_datetime(df['Fecha'])
@@ -40,3 +41,26 @@ def test_arima_grid_search(supply_df):
     assert isinstance(results['MAE'], float)
     assert isinstance(results['Params'], tuple)
     assert results['Name'] == 'ARIMA'
+
+def test_prophet_grid_search(supply_df):
+    """
+    Test the Grid Search on an Prophet model
+
+    Parameters
+    ----------
+    supply_df : pandas.DataFrame
+        DataFrame containing data to test the models
+    """
+    # Creates train and test data
+    train_data = supply_df[:-48]
+    test_data = supply_df[-48:]
+
+    # ARIMA Grid Search with a range of paremeters of 0
+    arima_grid_search = ProphetGridSearch()
+
+    results = arima_grid_search.grid_search(train_data, test_data)
+
+    # Check the dictionary elements
+    assert isinstance(results['MAE'], float)
+    assert isinstance(results['Params'], tuple)
+    assert results['Name'] == 'Prophet'
