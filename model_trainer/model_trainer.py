95996abb317e73e828ddaa521c2a3e4345139fb2
hectorLop <lopez.almazan.hector@gmail.com>
2020-07-29 10:19:47 +0200

    Added generate_train_and_test_sets() method

--- model_trainer/model_trainer.py
+++ model_trainer/model_trainer.py
@@ -1,10 +1,16 @@
-from models.custom_estimators import ARIMAEstimator, ProphetEstimator
+from .grid_search import GridSearch, ARIMAGridSearch, ProphetGridSearch
+from typing import Tuple
 import pandas
 
 class ModelTrainer():
     """
     This class represents a ModelTrainer to obtain the best
     possible model and its parameters
+
+    Parameters
+    ----------
+    data : pandas.DataFrame
+        DataFrame containing the whole data
 
     Attributes
     ----------
@@ -20,11 +26,10 @@ class ModelTrainer():
     - FBProphet
     """
 
-    def __init__(self) -> None:
-        self._train_data = None
-        self._test_data = None
+    def __init__(self, data: pandas.DataFrame) -> None:
+        self._train_data, self._test_data = self.__generate_train_and_test_sets(data)
 
-    def grid_search(train_data: pandas.DataFrame, test_data: pandas.DataFrame) -> dict:
+    def grid_search(self, train_data: pandas.DataFrame, test_data: pandas.DataFrame) -> dict:
         """
         Apply GridSearch on different models to obtain the best one
 
@@ -43,7 +48,7 @@ class ModelTrainer():
         """
         pass
 
-    def __generate_train_and_test_sets(data: pandas.DataFrame) -> tuple:
+    def __generate_train_and_test_sets(self, data: pandas.DataFrame) -> Tuple[pandas.DataFrame, pandas.DataFrame]:
         """
         Split the data into train and test sets
 
@@ -54,45 +59,12 @@ class ModelTrainer():
 
         Returns
         -------
-        train_data, test_data : tuple(pandas.DataFrame)
+        train_data, test_data : tuple(pandas.DataFrame, pandas.DataFrame)
             Two DataFrames containing the train and test data
         """
-        pass
+        # Assuming hourly frequency, the train data is the full dataset less 2 days
+        # which are the test data
+        train_data = data.iloc[:-48, :].copy()
+        test_data = data.iloc[-48:, :].copy()
 
-    def __grid_search_ARIMA(train_data: pandas.DataFrame, test_data: pandas.DataFrame) -> dict:
-        """
-        Apply GridSearch with an ARIMA model to obtain the model's best parameters
-
-        Parameters
-        ----------
-        train_data : pandas.DataFrame
-            DataFrame containing the training data for the model
-
-        test_data : pandas.DataFrame
-            DataFrame containing the test data for the model
-
-        Returns
-        -------
-        results : dict
-            Dictionary containing the best model with its parameters and metrics
-        """
-        pass
-
-    def __grid_search_Prophet(train_data: pandas.DataFrame, test_data: pandas.DataFrame) -> dict:
-        """
-        Apply GridSearch with a Prophet model to obtain the model's best parameters
-
-        Parameters
-        ----------
-        train_data : pandas.DataFrame
-            DataFrame containing the training data for the model
-
-        test_data : pandas.DataFrame
-            DataFrame containing the test data for the model
-
-        Returns
-        -------
-        results : dict
-            Dictionary containing the best model with its parameters and metrics
-        """
-        pass
+        return train_data, test_data
