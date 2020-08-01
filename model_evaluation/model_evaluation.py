91c73b8b30115edd5633f844dfd51bdc64fadde5
hectorLop <lopez.almazan.hector@gmail.com>
2020-08-01 12:01:48 +0200

    Added measure_elapsed_time() method

--- model_evaluation/model_evaluation.py
+++ model_evaluation/model_evaluation.py
@@ -1,4 +1,9 @@
+from typing import Callable
 import pandas
+from ..models.custom_estimators import TimeSeriesEstimator
+from sklearn.metrics import mean_squared_error
+import time
+import numpy
 
 class ModelEvaluation():
     """
@@ -10,14 +15,47 @@ class ModelEvaluation():
     data : pandas.DataFrame
         DataFrame containing the whole dataset
 
+    model : TimeSeriesEstimator
+        Estimator to fit and make predictions
+
     Attributes
     ----------
     _data : pandas.DataFrame
         DataFrame containing the whole dataset
+
+    _model : TimeSeriesEstimator
+        Estimator to fit and make predictions
     """
     
-    def __init__(self, data: pandas.DataFrame) -> None:
+    def __init__(self, data: pandas.DataFrame, model: TimeSeriesEstimator) -> None:
         self._data = data
+        self._model = model
 
     def cross_validation(self):
         pass
+
+    def _get_metrics(self, real_values: numpy.ndarray, predictions: numpy.ndarray):
+        """
+        Measures some regression metrics
+        """
+        pass
+
+    def _measure_elapsed_time(self, function: Callable) -> float:
+        """
+        Measures the execution time of a given function
+
+        Parameters
+        ----------
+        function : Callable
+            Function to be executed
+
+        Returns
+        -------
+        end - start _ float
+            Elapsed time
+        """
+        start = time.time()
+        function()
+        end = time.time()
+
+        return end - start
