5f8283ae46b03f747767267850f31ff35d06350c
hectorLop <lopez.almazan.hector@gmail.com>
2020-08-13 11:49:17 +0200

    Added skeleton for ModelRegistry class

---
+++ model_registry/model_registry.py
@@ -0,0 +1,43 @@
+from typing import Any, Tuple
+import json
+
+class ModelRegistry:
+    """
+    This class represents a Model Registry to save models and have a log of it
+
+    Parameters
+    ----------
+    connection : object
+        Database session
+
+    table_name : str
+        Registry table name. Default is 'registry'
+
+    Attributes
+    ----------
+    _connection : object
+        Database session
+
+    _table_name : str
+        Registry table name
+    """
+
+    def __init__(self, connection: object, table_name='registry') -> None:
+        self._connection = connection
+        self._table_name = table_name
+
+    def _insert(self, values: Tuple) -> None:
+        pass
+
+    def _query(self, query: str, values=None) -> None:
+        pass
+
+    def _save_to_remote(path: str, filename: str, model: object) -> None:
+        pass
+
+    def publish_model(self, model: object, name: str, parameters: tuple, metrics: Any,
+                      training_time: float, dataset_range: str) -> None:
+        pass
+
+    def get_model_info(self, name: str) -> str:
+        pass
