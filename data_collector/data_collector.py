89bb9e8f8ca6fc531fb8c4d149297d7e6b7f6625
hectorLop <lopez.almazan.hector@gmail.com>
2020-08-27 11:39:57 +0200

    Added method get_previous_day_date()

--- data_collector/data_collector.py
+++ data_collector/data_collector.py
@@ -1,6 +1,6 @@
-from pymongo import MongoClient
 from ..bbdd.db_connector import DBConnector
 from ..bbdd.connectors import MongoConnector
+from datetime import date, datetime, time, timedelta
 
 class DataCollector:
     """
@@ -9,6 +9,7 @@ class DataCollector:
     """
 
     DB_INFO_PATH = 'bbdd/db_info.ini'
+    ENDPOINT_URL = 'https://demanda.ree.es/WSvisionaMovilesPeninsulaRest/resources/demandaGeneracionPeninsula?fecha='
 
     def __init__(self) -> None:
         self._connection = self._create_connection()
@@ -27,8 +28,25 @@ class DataCollector:
 
         return db_connector.connect_to_db(self.DB_INFO_PATH)
 
-    def retrieve_last_day(self):
-        pass
+    def retrieve_last_day(self) -> None:
+        """
+        Retrieves data from the previous day
+        """
+        previous_day_str = self._get_previous_day_date()
+
+    def _get_previous_day_date(self) -> str:
+        """
+        Gets the previous day date as a string
+
+        Returns
+        -------
+        previous_day_str : str
+            String representing the date of the previous day in the format YYYY-MM-DD
+        """
+        today = date.today()
+        previous_day = today - timedelta(days = 1)
+
+        return previous_day.strftime('%Y-%m-%d')
 
     def insert_raw_data(self, data: dict) -> None:
         """
