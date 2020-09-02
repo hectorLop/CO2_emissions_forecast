f648394b9d4007fd273994f0da1ba2db1fe99375
hectorLop <lopez.almazan.hector@gmail.com>
2020-09-02 12:34:38 +0200

    Refactored tests

--- model_trainer/tests/test_model_trainer.py
+++ model_trainer/tests/test_model_trainer.py
@@ -1,34 +1,62 @@
 import pytest
-from ..model_trainer import ModelTrainer
-from ..grid_search import ARIMAGridSearch, ProphetGridSearch
-from .test_grid_search import supply_df
-from ...models.custom_estimators import ARIMAEstimator, ProphetEstimator
 
-def test_grid_search_model_trainer(supply_df):
+from model_trainer.model_trainer import ModelTrainer
+from model_trainer.grid_search import ARIMAGridSearch, ProphetGridSearch
+from tests_fixtures.fixtures import supply_df
+from models.custom_estimators import ARIMAEstimator, ProphetEstimator
+
+def test_model_trainer_arima(mocker, supply_df):
     """
-    Test the ModelTrainer on the ARIMA and Prophet models
+    Test the ModelTrainer on the ARIMA model
 
     Parameters
     ----------
     supply_df : pandas.DataFrame
         DataFrame containing data to test the models
     """
-    # Creation of GridSearch object
-    arima_grid_search = ARIMAGridSearch(range_limit=1)
-    prophet_grid_search = ProphetGridSearch()
+    results = {
+        'MAE': 15.2,
+        'Model': ARIMAEstimator(),
+        'Name': 'ARIMA'
+    }
 
-    # Creation of ModelTrainer for ARIMA
+    arima_grid_search = ARIMAGridSearch(range_limit=1)
     model_trainer_arima = ModelTrainer(supply_df, arima_grid_search)
+
+    # Mocks the model grid search method which is already tested
+    mocker.patch('model_trainer.model_trainer.ARIMAGridSearch.grid_search', return_value=results)
     arima_results = model_trainer_arima.grid_search()
 
-    # Creation of ModelTrainer for Prophet
-    model_trainer_prophet = ModelTrainer(supply_df, prophet_grid_search)
-    prophet_results = model_trainer_prophet.grid_search()
-
+    assert isinstance(arima_results, dict)
+    assert arima_results['MAE'] == 15.2
     assert isinstance(arima_results['MAE'], float)
     assert isinstance(arima_results['Model'], ARIMAEstimator)
     assert arima_results['Name'] == 'ARIMA'
 
-    assert isinstance(prophet_results['MAE'], float)
+def test_model_trainer_prophet(mocker, supply_df):
+    """
+    Test the ModelTrainer on the Prophet model
+
+    Parameters
+    ----------
+    supply_df : pandas.DataFrame
+        DataFrame containing data to test the models
+    """
+    results = {
+        'MAE': 15.2,
+        'Model': ProphetEstimator(),
+        'Name': 'Prophet'
+    }
+
+    prophet_grid_search = ProphetGridSearch()
+    model_trainer_prophet = ModelTrainer(supply_df, prophet_grid_search)
+
+    # Mocks the model grid search method which is already tested
+    mocker.patch('model_trainer.model_trainer.ProphetGridSearch.grid_search', return_value=results)
+    prophet_results = model_trainer_prophet.grid_search()
+
+    assert isinstance(prophet_results, dict)
+    assert prophet_results
+    assert prophet_results['MAE'] == 15.2
     assert isinstance(prophet_results['Model'], ProphetEstimator)
     assert prophet_results['Name'] == 'Prophet'
