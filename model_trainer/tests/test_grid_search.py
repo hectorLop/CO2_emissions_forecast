b750d5e3520682062bea9c10b543409703ff510e
hectorLop <lopez.almazan.hector@gmail.com>
2020-09-02 12:02:15 +0200

    Refactored tests to mock expensive time-consuming methods

--- model_trainer/tests/test_grid_search.py
+++ model_trainer/tests/test_grid_search.py
@@ -1,10 +1,10 @@
 import pytest
-import pandas
-from ..grid_search import ARIMAGridSearch, ProphetGridSearch
-from ...tests_fixtures.fixtures import supply_df
-from ...models.custom_estimators import ARIMAEstimator, ProphetEstimator
 
-def test_arima_grid_search(supply_df):
+from model_trainer.grid_search import ARIMAGridSearch, ProphetGridSearch
+from tests_fixtures.fixtures import supply_df
+from models.custom_estimators import ARIMAEstimator, ProphetEstimator
+
+def test_arima_grid_search(mocker, supply_df):
     """
     Test the Grid Search on an ARIMA model
 
@@ -20,6 +20,11 @@ def test_arima_grid_search(supply_df):
     # ARIMA Grid Search with a range of paremeters of 0
     arima_grid_search = ARIMAGridSearch(range_limit=1)
 
+    # Mocks the ArimaEstimator fit, predict and score methods inside grid_search
+    mocker.patch('model_trainer.grid_search.ARIMAEstimator.fit')
+    mocker.patch('model_trainer.grid_search.ARIMAEstimator.predict')
+    mocker.patch('model_trainer.grid_search.ARIMAEstimator.score', return_value=15.2)
+
     results = arima_grid_search.grid_search(train_data, test_data)
 
     # Check the dictionary elements
@@ -27,7 +32,7 @@ def test_arima_grid_search(supply_df):
     assert isinstance(results['Model'], ARIMAEstimator)
     assert results['Name'] == 'ARIMA'
 
-def test_prophet_grid_search(supply_df):
+def test_prophet_grid_search(mocker, supply_df):
     """
     Test the Grid Search on an Prophet model
 
@@ -40,10 +45,15 @@ def test_prophet_grid_search(supply_df):
     train_data = supply_df[:-48]
     test_data = supply_df[-48:]
 
-    # ARIMA Grid Search with a range of paremeters of 0
-    arima_grid_search = ProphetGridSearch()
+    # Prophet Grid Search with a range of paremeters of 0
+    prophet_grid_search = ProphetGridSearch()
 
-    results = arima_grid_search.grid_search(train_data, test_data)
+    # Mocks the ProphetEstimator fit, predict and score methods inside grid_search
+    mocker.patch('model_trainer.grid_search.ProphetEstimator.fit')
+    mocker.patch('model_trainer.grid_search.ProphetEstimator.predict')
+    mocker.patch('model_trainer.grid_search.ProphetEstimator.score', return_value=15.2)
+
+    results = prophet_grid_search.grid_search(train_data, test_data)
 
     # Check the dictionary elements
     assert isinstance(results['MAE'], float)
