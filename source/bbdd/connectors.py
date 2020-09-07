21e2fda99c6a389d31cd30bdb16d058cb1fe944b
hectorLop <lopez.almazan.hector@gmail.com>
2020-09-07 19:54:01 +0200

    Added TimescaleConnector and implemented methods to insert_data in PostgresConnector and TimeScaleConnector

--- source/bbdd/connectors.py
+++ source/bbdd/connectors.py
@@ -19,13 +19,28 @@ class Connector(ABC):
         """
         pass
 
+    @abstractmethod
+    def insert_data(self, name: str, values: dict) -> None:
+        """
+        Inserts data from a dictionary into a table or a collection
+
+        Parameters
+        ----------
+        name : str
+            Table or collection name
+
+        values : dict
+            Dictionary containing values to be inserted
+        """
+        pass
+
 class PostgresConnector(Connector):
     """
     Connection to a PostgreSQL database
     """
 
     def __init__(self) -> None:
-        pass
+        self._connection = None
 
     def connect(self, config_parsed: dict) -> object:
         """
@@ -41,11 +56,74 @@ class PostgresConnector(Connector):
         connection : object
             Object which encapsulates a database session
         """
-        connection = psycopg2.connect(user=config_parsed['postgres']['user'],
+        self._connection = psycopg2.connect(user=config_parsed['postgres']['user'],
                                       password=config_parsed['postgres']['password'],
                                       host=config_parsed['postgres']['host'],
                                       port=config_parsed['postgres']['port'],
                                       database=config_parsed['postgres']['database'])
+        
+        return self._connection
+
+    def insert_data(self, table_name: str, values: dict) -> None:
+        """
+        Inserts data from a dictionary into a table
+
+        Parameters
+        ----------
+        name : str
+            Table name
+
+        values : dict
+            Dictionary containing values to be inserted
+        """
+        query = f"""
+            INSERT INTO {table_name}
+            VALUES (?, ?)
+        """
+
+        for key in values.keys():
+            self._query(query, (key, values[key]))
+
+    def _query(self, query: str, values=None) -> None:
+        """
+        Makes a query to the database
+
+        Parameters
+        ----------
+        query : str
+            Query as a string
+
+        values : tuple
+            Tuple containing the query values. Default is None
+        """
+        cursor = self._connection.cursor()
+        cursor.execute(query, values)
+        cursor.close()
+
+class TimescaleConnector(PostgresConnector):
+    """
+    Connection to a Timescale database.
+    """
+
+    def connect(self, config_parsed: dict) -> object:
+        """
+        Establish connection with a Timescale database
+
+        Parameters
+        ----------
+        config_parsed : dict
+            Dictionary containing database settings
+
+        Returns
+        -------
+        connection : object
+            Object which encapsulates a database session
+        """
+        connection = psycopg2.connect(user=config_parsed['timescale']['user'],
+                                      password=config_parsed['timescale']['password'],
+                                      host=config_parsed['timescale']['host'],
+                                      port=config_parsed['timescale']['port'],
+                                      database=config_parsed['timescale']['database'])
         
         return connection
 
