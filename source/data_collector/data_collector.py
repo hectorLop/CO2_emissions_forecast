01f856766c04f4d33c5fa8dc2fc178b40e90830f
hectorLop <lopez.almazan.hector@gmail.com>
2020-09-09 12:42:52 +0200

    Added update_db() method and refactored some code

--- source/data_collector/data_collector.py
+++ source/data_collector/data_collector.py
@@ -1,6 +1,6 @@
 from source.bbdd.db_connector import DBConnector
 from source.bbdd.connectors import TimescaleConnector
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 import requests
 import json
 from typing import Dict, List, Tuple
@@ -60,9 +60,39 @@ class DataCollector:
         endpoint = self.ENDPOINT_URL + today_str
         # Retrieves the data from the endpoint
         energy_data = self._retrieve_energy_data(endpoint)
-        # Generates the emissions data from the energy_data
-        emissions = self._generate_emissions(energy_data)
+
+        # Uses the last 12 elements which are the last 2 hours due to the data
+        # is in a 10 minutes time format
+        emissions = self._generate_emissions(energy_data[-12:])
         
+        return emissions
+
+    def setup_db(self) -> None:
+        """
+        Configure the database with the emissions data.
+        If the database is empty, it gets the data from 2015-01-01 and
+        if not, it gets them since the last update
+        """
+        # Gets the last row to get the last update date
+        last_row = self._db_connector.select_last_row('emissions')
+
+        # Gets both start and end dates
+        start_date = self._get_start_date(last_row)
+        stop_date = datetime.now().date()
+        
+        emissions = []
+
+        while start_date <= stop_date:
+            # Generates the endpoint
+            start_date_str = start_date.strftime(self.DATE_FORMAT)
+            endpoint = self.ENDPOINT_URL + start_date_str
+            # Gets the data from that day
+            energy_data = self._retrieve_energy_data(endpoint)
+            # Compute the emissions and extends it to the list
+            emissions.extend(self._generate_emissions(energy_data))
+
+            start_date = start_date + timedelta(days=1)
+
         return emissions
 
     def _generate_today_date(self) -> str:
@@ -103,17 +133,15 @@ class DataCollector:
 
         json_data = json.loads(data)
 
-        # Returns the last 12 elements which are the last 2 hours due to the data
-        # is in a 10 minutes time format
-        return json_data[-12:]
+        return json_data
 
     def _generate_emissions(self, json_data: List[Dict]) -> List[Tuple]:
         """
         Generates a new list which contains the emissions for each timestamp.
         The list has the following format e.g :
         [
-            ('2020-08-27 21:00', 1000),
-            ('2020-08-27 21:10', 1200),
+            ('2020-08-27', '21:00', 1000),
+            ('2020-08-27', '21:10', 1200),
             ...
         ]
 
@@ -127,9 +155,10 @@ class DataCollector:
         emissions : List[Tuple]
             List of tuples. Each tuples represents one observation
         """
-        # Creates a list of tuples with the format (time, emission_value)
-        emissions = [(observation['ts'], self._compute_emissions(observation)) for observation in json_data]
-
+        # Creates a list of tuples with the format (date, hours, emission_value)
+        # Compute_emissions output is stored in a list so that the lists can be added
+        emissions = [tuple(observation['ts'].split() + [self._compute_emissions(observation)]) for observation in json_data]
+         
         return emissions
 
     def _compute_emissions(self, observation: dict) -> float:
@@ -156,3 +185,26 @@ class DataCollector:
         total_emissions = sum(emissions)
 
         return total_emissions
+
+    def _get_start_date(self, row: List[Tuple]) -> date:
+        """
+        Gets the date from a given row of the database
+
+        Parameters
+        ----------
+        row : List[tuple]
+            List containing a tuple for each row
+
+        Returns
+        -------
+        date : date
+            Date value 
+        """
+        # If row is empty, it means the table is empty
+        if not row:
+            date = datetime.strptime('2015-01-01', self.DATE_FORMAT).date()
+        else:
+            # Obtains the date column from the first row
+            date = row[0][0]
+
+        return date
