53e5baeb018ed40a1e1582f37258e8f71fcff230
hectorLop <lopez.almazan.hector@gmail.com>
2020-11-03 19:29:00 +0100

    Added endpoints to collect data, update the database and schedule a cron job

--- source/api/data_collector_api.py
+++ source/api/data_collector_api.py
@@ -1,10 +1,43 @@
 from flask import Flask
+from source.data_collector.data_collector import DataCollector
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+data_collector = DataCollector()
 
 @app.route("/")
-def hello_world():
+def hello_world() -> str:
     return 'Hello this is the data colletor API!'
 
+@app.route('/update_db')
+def update_db() -> str:
+    """
+    Update the database to keep it up to date
+    """
+    print("UPDATING DATABASE")
+    data_collector.collect_outdated_data()
+
+    return 'OK'
+
+def collect_data() -> str:
+    """
+    Collects emissions data
+    """
+    print('Collecting data...')
+    data_collector.collect_data()
+
+    return 'OK'
+
+def schedule_data_collection() -> None:
+    """
+    Schedule a job to collect emissions data each 6 hours
+    """
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=collect_data, trigger='interval', minutes=360)
+    scheduler.start()
+
+    print('SCHEDULER created')
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='localhost', port=8000)
+    schedule_data_collection()
